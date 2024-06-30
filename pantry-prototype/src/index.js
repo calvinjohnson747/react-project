db439294084a2d4e433dc3c9f243e535e1c5f7fb
calvinjohnson747 <147792607+calvinjohnson747@users.noreply.github.com>
2024-06-30 12:51:32 -0400

    NavBar Update

--- pantry-prototype/src/index.js
+++ pantry-prototype/src/index.js
@@ -3,9 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import NavBar from './navBar';
 import reportWebVitals from './reportWebVitals';
-import Login from './login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- pantry-prototype/src/navBar.js
+++ pantry-prototype/src/navBar.js
@@ -2,27 +2,33 @@ import './navBar.css'
 import { Link } from 'react-router-dom'
 //import { useNavigate } from 'react-router-dom';
 
-function NavBar( isAuth ){
-  //const navigate = useNavigate();
+function NavBar({ isAuth }){
     return(
-    <header className="navbar navbar-expand-lg navbar-light bg-light custom-navbar">
-    <div className="container-fluid">
-      <Link className="navbar-brand" to="/">Pantry 3427</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto">
+      <header className="navbar navbar-light bg-light custom-navbar">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Pantry 3427</Link>
+        <nav className="navbar-nav ms-auto d-flex flex-row">
           <li className="nav-item">
-            {isAuth && <Link className="nav-link" to="/pantry">Pantry</Link>}
+            <Link className="nav-link" to="/">Home</Link>
           </li>
-          <li className="nav-item">
-            {isAuth && <Link className="nav-link" to="/cart">Shopping Cart</Link>}
-          </li>
-        </ul>
+          {isAuth && (
+            <>
+              <li className="nav-item">
+                <Link className="nav-link" to="/pantry">Pantry</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">Shopping Cart</Link>
+              </li>
+            </>
+          )}
+          {!isAuth && (
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
+          )}
+        </nav>
       </div>
-    </div>
-  </header>
+    </header>
     );
   }
 
